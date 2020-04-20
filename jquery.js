3651f4850f82dffeedcc5dcc2a1ec32846cca5ff
Skander <62107377+Skander-benRomdhane@users.noreply.github.com>
2020-04-20 22:29:07 +0100

    first comment

---
+++ jquery.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+ $("header nav ul li a").hover(
+      function() {
+    $( this ).css("color", "blue");
+    },
+      function() {
+    $( this ).css("color", "purple");    
+      }
+    );
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//black bmw https://www.normandy-avenue.com/20-home_default/bmw-serie-5-berline.jpg
+
+//blue bmw https://www.motortrend.com/uploads/sites/10/2016/05/2016-bmw-active-hybrid-luxury-sedan-angular-front.png
+
+//black mercedes https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxUbDfy4BclceAWZYkbNT7nuLNxcvI6Q4yqyLm4ESBGY0bKhYB&usqp=CAU
+
+//gray mercedes https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBPnESqi9g9j4ZRC7diRdYBNR8m21yaZtufaiG_Jn0VMNk_g6o&usqp=CAU
